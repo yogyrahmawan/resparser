eb53d2a2a1f732d5a0b2f401acc8e785f4c0a288
yogy fresta rahmawan <yogy.frestarahmawan@gmail.com>
2024-07-27 14:26:05 +0700

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,14 @@ pub mod parser;
 
 #[cfg(test)]
 mod tests {
+    use parser::{parse, RespType};
+
     use super::*;
 
     #[test]
-    fn it_works() {}
+    fn test_simple_string() {
+        assert_eq!(parse("+OK\r\n"), Ok(("", RespType::SimpleString("OK"))));
+        assert!(parse("+O\nK\r\n").is_err());
+        assert!(parse("+OK\n\r").is_err());
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,72 +1,38 @@
 use core::str;
-use std::borrow::Cow;
 
-use nom::character::complete::{crlf, line_ending, not_line_ending, one_of};
+use nom::branch::alt;
+use nom::character::complete::{crlf, not_line_ending};
 use nom::character::streaming::char;
-use nom::combinator::map_res;
-use nom::complete::tag;
+use nom::combinator::map;
 use nom::sequence::delimited;
 use nom::IResult;
 
 #[derive(PartialEq, Clone, Debug, Hash)]
 pub enum RespType<'a> {
-    SimpleString(Cow<'a, str>),
-    Error(Cow<'a, str>),
-    BulkError(Cow<'a, str>),
+    SimpleString(&'a str),
+    Error(&'a str),
+    BulkError(&'a str),
     Integer(i64),
-    BulkString(Cow<'a, str>),
+    BulkString(&'a str),
     Array(Vec<RespType<'a>>),
     Null,
     Boolean(bool),
-    Double(Cow<'a, str>),
+    Double(&'a str),
     BigNumber(i128),
     Pushes,
 }
-
-impl<'a> RespType<'a> {
-    pub fn simple_string<T>(str: &'a T) -> Self
-    where
-        T: AsRef<str> + ?Sized,
-    {
-        Self::SimpleString(str.as_ref().into())
-    }
-}
-
 pub fn parse(data: &str) -> IResult<&str, RespType> {
-    let (data, resp_type) = one_of("+-:$*_#,(!=%~>")(data)?;
-    match resp_type {
-        '+' => parse_simple_string(data),
-        '-' => parse_simple_error(data),
-        ':' => todo!(),
-        '$' => todo!(),
-        '*' => todo!(),
-        '_' => todo!(),
-        '#' => todo!(),
-        ',' => todo!(),
-        '(' => todo!(),
-        '!' => todo!(),
-        '=' => todo!(),
-        '%' => todo!(),
-        '~' => todo!(),
-        '>' => todo!(),
-        _ => unreachable!("invalid resp_type"),
-    }
+    alt((parse_simple_string, parse_simple_error))(data)
 }
 
 fn parse_simple_string(data: &str) -> IResult<&str, RespType> {
-    let (data, result) = map_res(
-        delimited(char('+'), line_ending, crlf),
-        |s: &str| -> Result<RespType, std::convert::Infallible> { Ok(RespType::simple_string(s)) },
-    )(data)?;
-
-    Ok((data, result))
+    map(delimited(char('+'), not_line_ending, crlf), |s: &str| {
+        RespType::SimpleString(s)
+    })(data)
 }
 
 fn parse_simple_error(data: &str) -> IResult<&str, RespType> {
-    let (data, result) = map_res(
-        delimited(char('-'), line_ending, crlf),
-        |s: &str| -> Result<RespType, std::convert::Infallible> { Ok(RespType::simple_string(s)) },
-    )(data)?;
-
-    Ok((data, result))
+    map(delimited(char('-'), not_line_ending, crlf), |s: &str| {
+        RespType::SimpleString(s)
+    })(data)
 }
